ff2803dac15c14d1539a9dcdd3e296589dd181fc
Zuhaib <theshahzuhaib96@gmail.com>
2022-01-10 20:52:06 +0530

    ACP

--- src/app.js
+++ src/app.js
@@ -11,10 +11,8 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 const viewsPath = path.join(__dirname, '../templates/views');
 // const partialsPath = path.join(__dirname, '../templates/partials');
 
-// Setup handlebars engine and views location
 app.set('view engine', 'ejs');
 app.set('views', viewsPath);
-// hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
